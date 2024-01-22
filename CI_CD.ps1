fd74b6977dd8d1720e25083383ce23b8843c146a
onnig <brulez@insa-toulouse.fr>
2024-01-22 11:56:16 +0100

    test-push-ftp-edit

--- CI_CD.ps1
+++ CI_CD.ps1
@@ -52,3 +52,14 @@ foreach ($file in $files) {
 }
 
 Write-Host "Files uploaded successfully!"
+
+# SSH into remote Ubuntu server and replace IP address in files
+$remoteServer = "10.8.0.10"
+$userName = "waterleak"
+$password = "waterleak"
+
+$session = New-SSHSession -ComputerName $remoteServer -Credential (New-Object System.Management.Automation.PSCredential ($userName, (ConvertTo-SecureString $password -AsPlainText -Force))) -KeyFile "/path/to/private/key"
+
+Invoke-SSHCommand -SessionId $session.SessionId -Command "find /home/waterleak -type f -exec sed -i 's/10\.8\.0\.4/10.8.0.10/g' {} +"
+
+Remove-SSHSession -SessionId $session.SessionId
